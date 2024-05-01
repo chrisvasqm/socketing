69b59e78559da90fb5be7adca3eec5275d23d845
chrisvasqm <christianvasquezmelo@gmail.com>
2024-05-01 18:06:51 -0400

    Add server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const WebSocket = require('ws');
+
+const PORT = 8080;
+
+const server = new WebSocket.Server({ port: PORT }, () => {
+    console.log(`Socket API is listening on ws://localhost:${PORT}`)
+});
+
+server.on('connection', socket => {
+    socket.on('message', message => {
+        socket.send(`Roger that! ${message}`)
+    })
+})
